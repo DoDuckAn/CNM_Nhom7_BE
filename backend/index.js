7947a5eb6f9e290c4c2ee875f4525f1252678ed4
DoDuckAn <doducan13200401@gmail.com>
2025-03-27 16:27:37 +0700

    fix: sửa đúng tên key

--- backend/index.js
+++ backend/index.js
@@ -42,9 +42,9 @@ connectDB();
 io.use((socket,next)=>{
     const token=socket.handshake.auth.token;
     if(!token)
-        return next(new Error('Athentication error'));
+        return next(new Error('Authentication error'));
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
         if (err) 
             return next(new Error('Invalid token'));
 
